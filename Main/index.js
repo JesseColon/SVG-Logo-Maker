d7d993300e47f98a1aee3f51efe8e7b6292313bd
JesseColon <jay.alexandervii@gmail.com>
2023-05-21 21:15:51 -0400

    organized / began making switch case funcs

--- Main/index.js
+++ Main/index.js
@@ -1,5 +1,34 @@
-const CLI = require('./lib/cli.js');
+const inquirer = require('inquirer');
 
-const cli = new CLI();
 
-cli.run();
+const { writeFile } = require('fs').promises;
+// TODO: Create an array of questions f.or user input
+const userPrompt = () =>{
+  return  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'text',
+          message: 'enter 3 letters for your',
+        },
+        {
+          type: 'list',
+          name: 'color',
+          message: 'choose a color' ,
+          choices: ['blue, green, red, yellow, purple, orange']
+        },
+        {
+          type: 'list',
+          name: 'shape',
+          message: 'pick a shape',
+          choices: ['circle', 'triangle', 'square']
+        },
+        {
+          type: 'list',
+          name: 'shapeColor',
+          message: 'pick the shapes color',
+          choices: ['blue, green, red, yellow, purple, orange']
+        },
+    
+       
+    ])
+};
