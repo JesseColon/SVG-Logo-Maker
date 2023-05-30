8388dfde1d5ce262300f99a83fe7488455243fe8
JesseColon <jay.alexandervii@gmail.com>
2023-05-30 17:35:11 -0400

    running tests

--- Main/tests/shapes.test.js
+++ Main/tests/shapes.test.js
@@ -1,4 +1,25 @@
 const { generateLogo, renderShape } = require('../lib/GenerateLogo');
-const Circle = require('../circle');
-const Square = require('../square');
-const Triangle = require('../triangle');
+const Circle = require('../lib/circle');
+const Square = require('../lib/square');
+const Triangle = require('../lib/triangle');
+
+test('renderShape returns the correct shape instance for circle', () => {
+    const circleShape = renderShape('circle');
+    expect(circleShape).toBeInstanceOf(Circle);
+  });
+  
+  test('renderShape returns the correct shape instance for square', () => {
+    const squareShape = renderShape('square');
+    expect(squareShape).toBeInstanceOf(Square);
+  });
+  
+  test('renderShape returns the correct shape instance for triangle', () => {
+    const triangleShape = renderShape('triangle');
+    expect(triangleShape).toBeInstanceOf(Triangle);
+  });
+  
+  test('renderShape returns an empty string for unknown shape', () => {
+    const unknownShape = renderShape('unknown');
+    expect(unknownShape).toBe('');
+  });
+  
